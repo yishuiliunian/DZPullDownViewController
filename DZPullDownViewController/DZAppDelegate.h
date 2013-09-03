97d376d78310b9ef6436157cf840953ef3f8abbb
dzpqzb <dz@wiz.cn>
2013-09-03 20:26:59 +0800

    Initial Commit

---
+++ DZPullDownViewController/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  DZPullDownViewController
+//
+//  Created by dzpqzb on 13-9-3.
+//  Copyright (c) 2013年 dzpqzb inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
